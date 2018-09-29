564cc79534ea6553abb7ecb83c039a6e3d7ad445
Bil369 <jhsxszx@126.com>
2018-09-29 23:22:24 +0800

    Day4 Some tests
    Some tests in the codes I wrote yesterday.

--- random_walk/random_walk.py
+++ random_walk/random_walk.py
@@ -5,7 +5,7 @@ from random import choice
 
 class RandomWalk():
 
-    def __init__(self, num_points=5000):
+    def __init__(self, num_points=50000):
         self.num_points = num_points
         self.x_values = [0]
         self.y_values = [0]
@@ -19,6 +19,9 @@ class RandomWalk():
             y_direction = choice([1, -1])
             y_distance = choice([0, 1, 2, 3, 4])
             y_step = y_direction * y_distance
+
+            if x_step == 0 and y_step == 0:
+                continue
 
             next_x = self.x_values[-1] + x_step
             next_y = self.y_values[-1] + y_step
